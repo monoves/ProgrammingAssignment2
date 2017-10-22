b3f0a40976273f2a32f2eabacba195fbfba73641
monoves <mon.oves@gmail.com>
2017-10-22 17:10:32 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,5 @@
 ##The purpose of this is to: 1. set the matrix, 2. get the matrix, 3. set the inverse, 4. get the inverse
-## modeled after the mean example
+## modeled after the mean example, in order to return a reverse matrix
 makeCacheMatrix <- function(x = matrix()) {
   inv <- NULL
   set <- function(y){
@@ -14,7 +14,7 @@ makeCacheMatrix <- function(x = matrix()) {
      s.inv = s.inv, 
      g.inv = g.inv)
 }
-## calculates the inverse matrix
+## calculates the inverse matrix using the cache
 cacheInverse <- function(x, ...) {
   inv <- x$g.inv()
   if (!is.null(inv)){
@@ -24,5 +24,5 @@ cacheInverse <- function(x, ...) {
   data <- x$get() #the data of the matrix is called in order to then find the matrix
   inv <- solve(data, ...)
   x$s.inv(inv) ##where the made inverse is then called back
-  return(inv) ##and returned
+  return(inv) ##and returned, printing
 }
